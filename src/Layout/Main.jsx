cb793b7c53d9723258d03f389ac3d067f5f29586
afrin823 <afrin12490@gmail.com>
2024-07-10 20:24:52 +0600

    navbar compelete

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,10 +1,18 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Pages/Shard/Footer";
+import Navbar from "../Pages/Shard/Navbar";
+
+
+
+
 
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../Pages/Home/Home";
 import Main from "../Layout/Main";
-import Home from "../Pages/Home";
 
 
 
